e646c33ea9d3ac82e821970cef4977e531326f35
The Jay <jay@jayturn.com>
2022-06-03 14:32:58 +1000

    Add MUX destination
    * Add MUX as an option for video output destinations
    * Remove the EndpointsAPI from the SDK

--- shotstack_sdk/models/__init__.py
+++ shotstack_sdk/models/__init__.py
@@ -9,6 +9,7 @@
 # import sys
 # sys.setrecursionlimit(n)
 
+from shotstack_sdk.model.asset import Asset
 from shotstack_sdk.model.asset_render_response import AssetRenderResponse
 from shotstack_sdk.model.asset_response import AssetResponse
 from shotstack_sdk.model.asset_response_attributes import AssetResponseAttributes
@@ -16,6 +17,7 @@ from shotstack_sdk.model.asset_response_data import AssetResponseData
 from shotstack_sdk.model.audio_asset import AudioAsset
 from shotstack_sdk.model.clip import Clip
 from shotstack_sdk.model.crop import Crop
+from shotstack_sdk.model.destinations import Destinations
 from shotstack_sdk.model.edit import Edit
 from shotstack_sdk.model.flip_transformation import FlipTransformation
 from shotstack_sdk.model.font import Font
@@ -23,6 +25,8 @@ from shotstack_sdk.model.html_asset import HtmlAsset
 from shotstack_sdk.model.image_asset import ImageAsset
 from shotstack_sdk.model.luma_asset import LumaAsset
 from shotstack_sdk.model.merge_field import MergeField
+from shotstack_sdk.model.mux_destination import MuxDestination
+from shotstack_sdk.model.mux_destination_options import MuxDestinationOptions
 from shotstack_sdk.model.offset import Offset
 from shotstack_sdk.model.output import Output
 from shotstack_sdk.model.poster import Poster
